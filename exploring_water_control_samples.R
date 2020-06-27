b7e28dc4065df756f73012140a9cce4ca581fde3
Gray Ryan <mryan1@islander.tamucc.edu>
2020-06-27 17:43:40 -0500

    updated for new computer and added data that's up to date as of 6/27/2020

--- exploring_water_control_samples.R
+++ exploring_water_control_samples.R
@@ -6,6 +6,8 @@ watercontrols_bysamp <- read.delim("C:/Users/Gray Ryan.000/Desktop/Geist Lab/Dat
 #rename group names because this is bullshit
 watercontrols_bysamp$ctrl.set <- paste("g", watercontrols_bysamp$ctrl.set, sep="")
 
+library(ggplot2)
+
 #############################################################################################
 
 #testing purposes
@@ -59,12 +61,57 @@ color.df
 
 #############################################################################################
 
-
-
 #plotting occurance of each color in each control group
 
-ggplot(data) +
-  geom_bar( aes(x=name, y=value), stat="identity", fill="skyblue", alpha=0.7) +
-  geom_errorbar( aes(x=name, ymin=value-sd, ymax=value+sd), width=0.4, colour="orange", alpha=0.9, size=1.3)
+file.by.sample <- watercontrols_bysamp
+file.by.mp <- watercontrols_bymp
+color.vector <- get.mp.color.vector()
+color.header <- watercontrols_bymp$color
+group.vector <- c("g1", "g2", "g3")
+group.header <- watercontrols_bysamp$ctrl.set
+sample.name.header.mp <- watercontrols_bymp$ctrl.id
+sample.name.header.sample <- watercontrols_bysamp$ctrl.id
 
+color.df <- build.df.of.color.aves.for.ggplot(file.by.sample, 
+                                              file.by.mp,
+                                              color.vector,
+                                              color.header,
+                                              group.vector, 
+                                              group.header,
+                                              sample.name.header.mp,
+                                              sample.name.header.sample)
+color.df
 
+p<- ggplot(color.df, aes(x=group, y=average, fill=color)) + 
+  geom_bar(stat="identity", color="black", 
+           position=position_dodge()) +
+  geom_errorbar(aes(ymin=average-sd, ymax=average+sd), width=.2,
+                position=position_dodge(.9)) 
+print(p)
+
+#######################################################################################################
+
+group1.samp <- watercontrols_bysamp[watercontrols_bysamp$ctrl.set=="g1",]
+group1.samp[is.na(group1.samp)] = 0
+group1.samp
+
+group1.mp <- watercontrols_bymp[watercontrols_bymp$ctrl.set=="1",]
+group1.mp[is.na(group1.mp)] = 0
+group1.mp
+group1.mp[101:153,]
+
+group2.samp <- watercontrols_bysamp[watercontrols_bysamp$ctrl.set=="g2",]
+group2.samp[is.na(group2.samp)] = 0
+group2.samp
+
+group2.mp <- watercontrols_bymp[watercontrols_bymp$ctrl.set=="2",]
+group2.mp[is.na(group2.mp)] = 0
+group2.mp
+
+group3.samp <- watercontrols_bysamp[watercontrols_bysamp$ctrl.set=="g3",]
+group3.samp[is.na(group3.samp)] = 0
+group3.samp
+
+group3.mp <- watercontrols_bymp[watercontrols_bymp$ctrl.set=="3",]
+group3.mp[is.na(group3.mp)] = 0
+group3.mp
--- master_script_v2.R
+++ master_script_v2.R
@@ -10,20 +10,20 @@
 
 get.mp.type.vector <- function (){
   #this function returns a vector of every type of microplastic
-  c(fib, flm, prt)
+  c("fib", "flm", "prt")
 }
 
 #############################################################################################
 
 get.mp.color.vector <- function (){
   #this function returns a vector of every color of microplastic
-  c(red, orn, yel, grn, blu, pur, pnk, clr, blk, wht, gry, brn, tan)
+  c("red", "orn", "yel", "grn", "blu", "pur", "pnk", "clr", "blk", "wht", "gry", "brn", "tan")
 }
 
 #############################################################################################
 
 string.to.function <- function (vector.of.strings){
-  #takes a vector of strings and runs them all pasted together as code
+  #takes a vector of strings and pastes them all together to be run as code
   final.string<-""
   i<-1
   while (i<=length(vector.of.strings)){
@@ -110,7 +110,8 @@ sep.vect <- function(vector){
     } else {
       final.str <- paste(final.str, vector[place], sep=",")
     }
-    place <- place+1
+    place <- place+1`, BNM,./"
+    "/;.L,KMNBV C]'[;PLOKIJHGFDCSXZXDCFVBGJKILO;P'[\=-097Y654R3ES2WQWSE34R5TY6790-=-0987890-=-0`1
   }
   final.str <- paste(final.str, ")", sep="")
   final.str
@@ -154,7 +155,6 @@ build.df.of.color.aves <- function(file.by.sample,
       final.df <- cbind(final.df, new.final.df.col)
       place.in.color.vector <- place.in.color.vector + 1
     }
-    final.df
     
   #fill in df with average occurance of each color of mp, separated by group
     color.index <- 2
@@ -175,10 +175,175 @@ build.df.of.color.aves <- function(file.by.sample,
       }
       color.index <- color.index + 1
     }
+    
 final.df
 }
 
+######################################################################################################
 
+reorg.col.df <- function (df.of.color.aves){
+  
+  #df.of.color.aves: a df in which each group is a row and each column is a color and the values are 
+    #the average number of occurances of that color in samples in that group
+  #this function reorganizes that dataframe to make color its own column
+  
+  #get color names from column headings
+  cols <- colnames(df.of.color.aves)
+  cols <- cols[2:length(cols)]
+  
+  #get group names from rows
+  groups <- df.of.color.aves$group
+  
+  #create vectors for each column
+  new.group.vec <- c() 
+  new.color.vec <- c() 
+  new.average.vec <- c() 
+  
+  numcols <- length(cols)
+  numgroups <- nrow(df.of.color.aves)
+  
+  
+  
+  col.place <- 1
+  group.place <- 1
+  while (col.place <= numcols){
+    while (group.place <= numgroups){
+      this.ave <- df.of.color.aves[group.place, (col.place + 1)]
+      this.group <- groups[group.place]
+      this.color <- cols[col.place]
+      
+      new.group.vec <- append(new.group.vec, this.group, after = length(new.group.vec))
+      new.color.vec <- append(new.color.vec, this.color, after = length(new.color.vec))
+      new.average.vec <- append(new.average.vec, this.ave, after = length(new.average.vec))
+      
+      group.place <- group.place + 1
+    }
+    col.place <- col.place + 1
+    group.place <- 1
+  }
+  
+  new.df <- data.frame("group" = new.group.vec, "color" = new.color.vec, "average" = new.average.vec)
+  new.df
+}
 
+#####################################################################################################
 
+build.df.of.color.aves.for.ggplot <- function(file.by.sample, 
+                                              file.by.mp,
+                                              color.vector,
+                                              color.header,
+                                              group.vector, 
+                                              group.header,
+                                              sample.name.header.mp,
+                                              sample.name.header.sample) {
+  #file.by.sample: the name of the text file containing tidy data with sample as unit of mea-
+    #surement
+  #file.by.mp: the name of the text file containing tidy data with microplastic as unit of m-
+    #easurement
+  #color.vector: a vector of which colors of mp you want included in data frame 
+  #color.header: the column name where the color value can be found, including the file name
+    #(for example: data$color)
+  #group.vector: a vactor of which groups you want to include in the dataframe
+  #group.header:the column name where the group.name is located, including the file name (for
+    #example: data$group)
+  #sample.name.header.mp: column where sample names are stored in mp data, including file na-
+    #me (for example: data$sample.id)
+  #sample.name.header.mp: column where sample names are stored in sample data, including fil-
+    #e name (for example: data$sample.id)
+  
+  #this function creates a data frame in which the columns are "group", "color", "average", &
+    "sd"
+  
+  #create df using build.df.of.color.aves
+  new.df <- build.df.of.color.aves(file.by.sample, 
+                                   file.by.mp,
+                                   color.vector,
+                                   color.header,
+                                   group.vector, 
+                                   group.header,
+                                   sample.name.header.mp,
+                                   sample.name.header.sample)
+  
+  #create sd df using build.df.of.color.sds
+  new.df.sd <- build.df.of.color.sds(file.by.sample, 
+                                     file.by.mp,
+                                     color.vector,
+                                     color.header,
+                                     group.vector, 
+                                     group.header,
+                                     sample.name.header.mp,
+                                     sample.name.header.sample)
+  
+  #reorganize tables into format useful for ggplot
+  new.df <- reorg.col.df(new.df)
+  new.df.sd <- reorg.col.df(new.df.sd)
+  
+  #combine
+  final.df <- cbind(new.df, new.df.sd$average)
+  colnames(final.df) <- c("group", "color", "average", "sd")
+  final.df
+  
+}
 
+####################################################################################################
+
+build.df.of.color.sds <- function(file.by.sample, 
+                                   file.by.mp,
+                                   color.vector,
+                                   color.header,
+                                   group.vector, 
+                                   group.header,
+                                   sample.name.header.mp,
+                                   sample.name.header.sample) {
+  
+  #file.by.sample: the name of the text file containing tidy data with sample as unit of mea-
+  #surement
+  #file.by.mp: the name of the text file containing tidy data with microplastic as unit of m-
+  #easurement
+  #color.vector: a vector of which colors of mp you want included in data frame 
+  #color.header: the column name where the color value can be found, including the file name
+  #(for example: data$color)
+  #group.vector: a vactor of which groups you want to include in the dataframe
+  #group.header:the column name where the group.name is located, including the file name (for
+  #example: data$group)
+  #sample.name.header.mp: column where sample names are stored in mp data, including file na-
+  #me (for example: data$sample.id)
+  #sample.name.header.mp: column where sample names are stored in sample data, including fil-
+  #e name (for example: data$sample.id)
+  
+  #build empty df
+  final.df <- data.frame("group" = group.vector)
+  place.in.color.vector <- 1
+  while (place.in.color.vector <= length(color.vector)){
+    temp.zero.vect <- rep(0, length(group.vector))
+    temp.zero.vect <- sep.vect (temp.zero.vect)
+    temp.col.name <- color.vector[place.in.color.vector]
+    temp.text.to.run <- string.to.function(c("new.final.df.col <- data.frame (", temp.col.name, " = ", temp.zero.vect, ")"))
+    eval(parse(text=temp.text.to.run))
+    final.df <- cbind(final.df, new.final.df.col)
+    place.in.color.vector <- place.in.color.vector + 1
+  }
+  
+  #fill in df with average occurance of each color of mp, separated by group
+  color.index <- 2
+  for (color in color.vector){
+    group.index <- 1
+    for (group in group.vector){
+      temp.mp.vector <- create.vector.of.mp.values.for.group(file.by.sample, 
+                                                             file.by.mp,
+                                                             color,
+                                                             color.header,
+                                                             group, 
+                                                             group.header,
+                                                             sample.name.header.mp,
+                                                             sample.name.header.sample)
+      temp.mp.sd <- sd(temp.mp.vector)
+      final.df[group.index, color.index] <- temp.mp.sd
+      group.index <- group.index + 1
+    }
+    color.index <- color.index + 1
+  }
+  
+  final.df
+}
+  
